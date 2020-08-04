028f0facbc2641cd3767b9b8ecc0d3e4e16cecbf
Andrea Contestabile <andreacontest@gmail.com>
2020-08-04 16:02:44 +0200

    Calcolo costo biglietto del treno JS

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// 1. Chiediamo il numero di km da percorrere (e salviamo in una variabile)
+var kmUtente = parseInt(prompt("Per favore, inserisci il numero di chilometri da percorrere"));
+// 2. Chiediamo l'età del passeggero (e salviamo in una variabile)
+var etaUtente = parseInt(prompt("Per favore, inserisci la tua età"));
+// 3. Calcoliamo il prezzo del biglietto, secondo queste regole:
+  // Prezzo 0.21€/km
+  // Sconto 20% per i minorenni
+  // Sconto 40% per gli over 65
+var costoKm = 0.21;
+var costo = costoKm * kmUtente;
+
+if (etaUtente < 18) {
+  var sconto = 0.2;
+} else if (etaUtente >= 65) {
+  var sconto = 0.4;
+} else {
+  var sconto = 0;
+}
+
+var costoTotale = (costo - costo * sconto).toFixed(2);
+
+document.getElementById('km').innerHTML = kmUtente;
+document.getElementById('eta').innerHTML = etaUtente;
+document.getElementById('prezzo').innerHTML = costo;
+document.getElementById('sconto').innerHTML = sconto * 100;
+document.getElementById('totale').innerHTML = costoTotale;
